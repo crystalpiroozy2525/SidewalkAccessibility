f3d34315063c4b1bea9c8a785720f858af6c5b5c
Crystal Posey <85965431+crystalpiroozy2525@users.noreply.github.com>
2023-02-22 16:38:33 -0500

    Update SidewalkCalculateField.py
    The following script is used in the Calculate Field tool in ArcGIS Pro.

--- SidewalkCalculateField.py
+++ SidewalkCalculateField.py
@@ -4,6 +4,7 @@ Created on Jul 2022
 Calculate Function Python Script for Tyree's Sidewalk Project'
 
 @author: Crystal Posey
+
 """
 ##Input for the object field type. It takes the Reclass Function and its parameters.
 Reclass(!Cross_Slope_b!, !Cross_Slope_m!, !Cross_Slope_e!, !Roadway_Longitudinal_Slope_b!, !RoadwayLongslope__m!, !RoadwayLongslope__e!, !Sidewalk_Longitudinal_Slope_b!, !Sidewalkslope_m!, !Sidewalkslope_e!, !Widthofsidewakl_b!, !Narrowest!, !Widthofsidewalk_e!, !Ramp_Compliant_Accessible___Y_or_N_!, !Vertical_Clearance_Compliant_!, !Driveways_Compliant___Y_or_N_!, !Stairs_in_segment_!, !Any_Pedestrian_Barriers___stairs__trees__narrow_sidewalk__no_ram!)
